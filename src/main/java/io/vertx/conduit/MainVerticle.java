2692dd8e06626a2fd01b747caafb9a0064abafa0
jeremyrdavis <jeremyrdavis@jeremyrdavis.com>
2018-09-30 07:45:46 -0400

    test passing.  code needs to be cleaned up

--- src/main/java/io/vertx/conduit/MainVerticle.java
+++ src/main/java/io/vertx/conduit/MainVerticle.java
@@ -1,11 +1,16 @@
 package io.vertx.conduit;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.AsyncResult;
 import io.vertx.core.Future;
 import io.vertx.core.http.HttpServerResponse;
+import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
+import io.vertx.ext.auth.User;
 import io.vertx.ext.auth.jdbc.JDBCAuth;
 import io.vertx.ext.jdbc.JDBCClient;
+import io.vertx.ext.sql.ResultSet;
+import io.vertx.ext.sql.SQLConnection;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.RoutingContext;
 import io.vertx.ext.web.handler.BodyHandler;
@@ -47,6 +52,71 @@ public class MainVerticle extends AbstractVerticle {
         });
   }
 
+  private void loginHandler(RoutingContext context){
+    JsonObject user = context.getBodyAsJson().getJsonObject("user");
+    user.put("username", "placeholder");
+
+    JsonObject authInfo = new JsonObject()
+      .put("username", user.getString("email"))
+      .put("password", user.getString("password"));
+    System.out.println(user);
+
+    HttpServerResponse response = context.response();
+    authProvider.authenticate(authInfo, (AsyncResult<User> ar) -> {
+      if (ar.succeeded()) {
+        System.out.println("Authentication succeeded");
+        jdbcClient.getConnection(ar2 ->{
+          if (ar2.succeeded()) {
+            System.out.println("Connection succeeded");
+            SQLConnection connection = ar2.result();
+            connection.queryWithParams("SELECT * FROM USER WHERE EMAIL = ?", new JsonArray().add(user.getString("email")), fetch ->{
+              if (fetch.succeeded()) {
+                JsonObject res = new JsonObject();
+                ResultSet resultSet = fetch.result();
+                System.out.println("Query returned " + resultSet.getNumRows() + " results");
+                if (resultSet.getNumRows() == 0) {
+                  res.put("found", false);
+                  response.setStatusCode(200)
+                    .putHeader("Content-Type", "text/html")
+                    .putHeader("Content-Length", String.valueOf("Not Found".length()))
+                    .end(fetch.cause().toString());
+                } else {
+                  res.put("found", true);
+                  JsonArray row = resultSet.getResults().get(0);
+                  JsonObject retVal = new JsonObject();
+                  retVal.put("username", row.getString(1));
+                  retVal.put("email", row.getString(2));
+                  retVal.put("bio", row.getString(3));
+                  retVal.put("image", row.getString(4));
+                  res.put("user", retVal);
+                  response.setStatusCode(200)
+                    .putHeader("Content-Type", "application/json; charset=utf-8")
+                    .putHeader("Content-Length", String.valueOf(res.toString().length()))
+                    .end(res.encode());
+                }
+              } else{
+                response.setStatusCode(200)
+                  .putHeader("Content-Type", "text/html")
+                  .end(fetch.cause().toString());
+              }
+            });
+          }else{
+            response.setStatusCode(200)
+              .putHeader("Content-Type", "text/html")
+              .end(ar2.cause().toString());
+          }
+        });
+
+      }else{
+        response.setStatusCode(200)
+          .putHeader("Content-Type", "text/html")
+          .end("Authentication Failed: " + ar.cause());
+      }
+    });
+
+  }
+
+/*
   private void loginHandler(RoutingContext context) {
     JsonObject user = context.getBodyAsJson().getJsonObject("user");
     user.put("username", "placeholder");
@@ -55,27 +125,58 @@ public class MainVerticle extends AbstractVerticle {
       .put("username", user.getString("email"))
       .put("password", user.getString("password"));
 
-    System.out.println(user);
+    System.out.println("loginHandler: " + user);
 
     HttpServerResponse response = context.response();
+    String failureMessage = "Authentication Failed";
 
     authProvider.authenticate(authInfo, ar -> {
       if (ar.succeeded()) {
 
-        JsonObject returnValue = new JsonObject()
-          .put("user", new JsonObject()
-            .put("email", "jake@jake.jake")
-            .put("password", "jakejake")
-            .put("token", "jwt.token.here")
-            .put("username", "jake")
-            .put("bio", "I work at statefarm")
-            .put("image", ""));
-        System.out.println(returnValue);
+        System.out.println("Authentication succeeded");
 
-        response.setStatusCode(200)
-          .putHeader("Content-Type", "application/json; charset=utf-8")
-          .putHeader("Content-Length", String.valueOf(returnValue.toString().length()))
-          .end(returnValue.encode());
+        JsonObject returnValue = new JsonObject();
+
+        jdbcClient.getConnection(ar2 -> {
+          if (ar2.succeeded()) {
+
+            System.out.println("Connection established");
+
+            SQLConnection connection = ar2.result();
+            connection.queryWithParams("SELECT * FROM USER WHERE EMAIL = ?", new JsonArray().add(user.getString("email")), fetch ->{
+              if (fetch.succeeded()) {
+
+                System.out.println("User retrieved");
+
+                ResultSet resultSet = fetch.result();
+                if (resultSet.getNumRows() >= 1) {
+
+                  JsonArray row = resultSet.getResults().get(0);
+                  returnValue.put("username", row.getString(1));
+                  returnValue.put("email", row.getString(2));
+                  returnValue.put("bio", row.getString(3));
+                  returnValue.put("image", row.getString(4));
+
+                  System.out.println("returnValue: " + returnValue);
+
+                  response.setStatusCode(200)
+                    .putHeader("Content-Type", "application/json; charset=utf-8")
+                    .putHeader("Content-Length", String.valueOf(returnValue.toString().length()))
+                    .end(returnValue.encode());
+                }else{
+                  response.setStatusCode(500)
+                    .putHeader("Content-Type", "text/html")
+                    .end("No rows retrieved " + fetch.cause());
+                }
+              }else{
+                response.setStatusCode(500)
+                  .putHeader("Content-Type", "text/html")
+                  .end("Query failed " + fetch.cause());
+              }
+            });
+
+          }
+        });
       }else{
         response.setStatusCode(200)
           .putHeader("Content-Type", "text/html")
@@ -83,7 +184,7 @@ public class MainVerticle extends AbstractVerticle {
       }
     });
   }
-
+*/
   private void indexHandler(RoutingContext context) {
     HttpServerResponse response = context.response();
     response.putHeader("Content-Type", "text/html").end("Hello, CodeOne!");
--- src/test/java/io/vertx/conduit/AuthenticationEndpointTest.java
+++ src/test/java/io/vertx/conduit/AuthenticationEndpointTest.java
@@ -3,6 +3,7 @@ package io.vertx.conduit;
 import io.vertx.core.Vertx;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.client.WebClient;
+import io.vertx.ext.web.codec.BodyCodec;
 import io.vertx.junit5.VertxExtension;
 import io.vertx.junit5.VertxTestContext;
 import org.junit.jupiter.api.DisplayName;
@@ -10,6 +11,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @DisplayName("Authentication Endpoint Tests")
 @ExtendWith(VertxExtension.class)
@@ -20,24 +22,59 @@ public class AuthenticationEndpointTest {
   void testSuccessfulAuthentication(Vertx vertx, VertxTestContext testContext) throws InterruptedException {
 
     vertx.deployVerticle(new MainVerticle(), testContext.succeeding(id -> {
+
       WebClient webClient = WebClient.create(vertx);
+
+      webClient.post(8080, "localhost", "/api/users/login")
+          .sendJsonObject(new JsonObject()
+            .put("user", new JsonObject()
+              .put("email", "jake@jake.jake")
+              .put("password", "jakejake")
+            ), testContext.succeeding(resp -> {
+              testContext.verify(() -> {
+                assertEquals(200, resp.statusCode(), "status code should be 200");
+                JsonObject user = resp.bodyAsJsonObject().getJsonObject("user");
+                assertNotNull(user);
+                assertEquals("jake@jake.jake", user.getString("email"), "Email should be 'jake@jakejake'");
+                assertEquals("jake", user.getString("username"), "username should not be 'jake");
+                testContext.completeNow();
+              });
+          }));
+//        .send(testContext.succeeding(resp -> {
+//          testContext.verify(() -> {
+//            assertEquals(200, resp.statusCode(), "status code should be 200");
+//          });
+//        }));
+    }));
+
+/*
       webClient.post(8080, "localhost", "/api/users/login")
         .sendJsonObject(new JsonObject()
           .put("user", new JsonObject()
             .put("email", "jake@jake.jake")
             .put("password", "jakejake")
-          ), response -> testContext.verify(() -> {
-          JsonObject user = response.result().bodyAsJsonObject().getJsonObject("user");
-          assertEquals(200, response.result().statusCode());
-          assertEquals("jake@jake.jake", user.getString("email"));
-          assertEquals("jakejake", user.getString("password"));
-          assertEquals("jwt.token.here", user.getString("token"));
-          assertEquals("jake", user.getString("username"));
-          assertEquals("I work at statefarm", user.getString("bio"));
-          assertEquals("", user.getString("image"));
-          testContext.completeNow();
-        }));
+          ), response -> {
+
+          if (response.succeeded()) {
+            JsonObject user = response.result().bodyAsJsonObject().getJsonObject("user");
+            System.out.println("Response: " + user);
+            assertEquals(200, response.result().statusCode());
+            assertNotNull(user.getString("email"), "email should not be null");
+            assertEquals("jake@jake.jake", user.getString("email"));
+            assertNotNull(user.getString("password"), "password should not be null");
+            assertEquals("jakejake", user.getString("password"));
+            assertNotNull( user.getString("token"), "JWT Token should not be null");
+            assertEquals("jwt.token.here", user.getString("token"));
+            assertEquals("jake", user.getString("username"));
+            assertEquals(user.getString("username"), "username should not be null");
+            assertEquals("I work at statefarm", user.getString("bio"));
+            assertEquals("", user.getString("image"));
+            assertNotNull( user.getString("image"), "image should not be null");
+            testContext.completeNow();
+          }
+        });
     }));
+*/
   }
 
 }
